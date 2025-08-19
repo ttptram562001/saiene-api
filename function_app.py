3cb48a3c6d2cbda0a5e5ccfdd1edfff370e7b3fd
tramttp <phuongtram@techzen.vn>
2025-08-19 11:02:41 +0700

    test ci/cd

--- function_app.py
+++ function_app.py
@@ -8,6 +8,7 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    
     if not name:
         try:
             req_body = req.get_json()
