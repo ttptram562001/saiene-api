3546ce16f55d04d0fb140e3f2ee715ce2da8117b
tramttp <phuongtram@techzen.vn>
2025-08-19 13:24:11 +0700

    fix error deploy

--- function_app.py
+++ function_app.py
@@ -27,9 +27,7 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
 @app.route(route="HelloApi", auth_level=func.AuthLevel.ANONYMOUS)
 def HelloApi(req: func.HttpRequest) -> func.HttpResponse:
-    
     logging.info('Python HTTP trigger function processed a request.')
-
     name = req.params.get('name')
     if not name:
         try:
@@ -38,7 +36,6 @@ def HelloApi(req: func.HttpRequest) -> func.HttpResponse:
             req_body = {}
         else:
             name = req_body.get('name')
-
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
